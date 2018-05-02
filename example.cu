345e7b31d096225871f41d7640fce88eb159313a
idontcare1996 <cmbo1996@gmail.com>
2018-05-02 04:30:03 +0100

    Cleaned up the example code. Placed some simple "printf"'s

--- example.cu
+++ example.cu
@@ -1,97 +1,17 @@
 #include <stdio.h>
 
-//
-// Nearly minimal CUDA example.
-// Compile with:
-//
-// nvcc -o example example.cu
-//
 extern "C"
 {
     #include "qdbmp.h"
 }
-#define N 1000
 
-//
-// A function marked __global__
-// runs on the GPU but can be called from
-// the CPU.
-//
-// This function multiplies the elements of an array
-// of ints by 2.
-//
-// The entire computation can be thought of as running
-// with one thread per array element with blockIdx.x
-// identifying the thread.
-//
-// The comparison i<N is because often it isn't convenient
-// to have an exact 1-1 correspondence between threads
-// and array elements. Not strictly necessary here.
-//
-// Note how we're mixing GPU and CPU code in the same source
-// file. An alternative way to use CUDA is to keep
-// C/C++ code separate from CUDA code and dynamically
-// compile and load the CUDA code at runtime, a little
-// like how you compile and load OpenGL shaders from
-// C/C++ code.
-//
 __global__
-void add(int *a, int *b) {
-    int i = blockIdx.x;
-    if (i<N) {
-        b[i] = 2*a[i];
-    }
+void do_the_math ()
+{
+
 }
 
-int main() {
-    //
-    // Create int arrays on the CPU.
-    // ('h' stands for "host".)
-    //
-    int ha[N], hb[N];
-
-    //
-    // Create corresponding int arrays on the GPU.
-    // ('d' stands for "device".)
-    //
-    int *da, *db;
-    cudaMalloc((void **)&da, N*sizeof(int));
-    cudaMalloc((void **)&db, N*sizeof(int));
-
-    //
-    // Initialise the input data on the CPU.
-    //
-    for (int i = 0; i<N; ++i) {
-        ha[i] = i;
-    }
-
-    //
-    // Copy input data to array on GPU.
-    //
-    cudaMemcpy(da, ha, N*sizeof(int), cudaMemcpyHostToDevice);
-
-    //
-    // Launch GPU code with N threads, one per
-    // array element.
-    //
-    add<<<N, 1>>>(da, db);
-
-    //
-    // Copy output array from GPU back to CPU.
-    //
-    cudaMemcpy(hb, db, N*sizeof(int), cudaMemcpyDeviceToHost);
-
-    for (int i = 0; i<N; ++i) {
-        printf("%d\n", hb[i]);
-    }
-
-    //
-    // Free up the arrays on the GPU.
-    //
-    cudaFree(da);
-    cudaFree(db);
-
-    // Start stuff
+int main() {   
 
         BMP* bmp;
         UCHAR r, g, b;
@@ -100,6 +20,7 @@ int main() {
  
     
     /* Read an image file */
+    printf(" Reading the image file... \n");
     bmp = BMP_ReadFile( "image_input.bmp" );
     BMP_CHECK_ERROR( stderr, -1 ); 
     /* If an error has occurred, notify and exit */
@@ -119,6 +40,7 @@ int main() {
     }
     /* Save result */
     BMP_WriteFile( bmp, "image_output.bmp" );
+    printf(" Image output is ready \n");
     BMP_CHECK_ERROR( stderr, -2 );
     /* Free all memory allocated for the image */
     BMP_Free( bmp );    
